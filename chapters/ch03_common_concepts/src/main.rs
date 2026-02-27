d7924ce2bdcba9a76725969be6e0663ed0baba7b
Shiyo1101 <yoshiki1011abc@gmail.com>
2026-02-28 00:08:12 +0900

    ✨ ループを使ったカウント機能を実装

--- chapters/ch03_common_concepts/src/main.rs
+++ chapters/ch03_common_concepts/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Chapter 3: Common Programming Concepts (TODO)");
+    let mut count = 1;
+
+    loop {
+        println!("Cont: {count}");
+
+        if count % 3 == 0 {
+            println!("{count} is divisible by 3");
+        }
+
+        if count == 10 {
+            break;
+        }
+
+        count += 1;
+    }
 }
