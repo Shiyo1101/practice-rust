bed45cfe4f660bb35428c4a211b75c5b9f880718
Shiyo1101 <yoshiki1011abc@gmail.com>
2026-03-08 15:10:56 +0900

    ✨ 長方形の面積を計算する機能を実装

--- chapters/ch05_structs/src/main.rs
+++ chapters/ch05_structs/src/main.rs
@@ -1,3 +1,15 @@
+// Chapter 5: Structs (TODO)
+
 fn main() {
-    println!("Chapter 5: Structs (TODO)");
+    let width = 30;
+    let height = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width, height)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
